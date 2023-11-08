9f316b90cf371ae9df5484bcc7fa4da1a331e3f0
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-11-08 14:51:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,12 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}</b>""" 
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {full_file_name}
+
+<b> Size ⚙️: {file_size}
+
+<b> ⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
+    
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
