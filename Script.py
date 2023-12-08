4c800380a0bf68cc260aeb467ba23eaebbf59ec9
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-08 08:15:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : <b>{file_caption}<b>
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_caption}
 
 <b> Size ⚙️: {file_size}
 
