62c614320eac65a5da21320b70acb0705f598ac1
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-09 12:46:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,7 +33,7 @@ async def start(client, message):
                     InlineKeyboardButton('🔱 Support Group 🔱', url="https://t.me/Deendayal_dhakad_Group"),
                     InlineKeyboardButton('🌿 Movie Group 🌿', url='https://t.me/+UexCvjiPgXljNDRl')
                 ],[
-                  InlineKeyboardButton('⚜️ Join updates Channel ⚜️', url='https://t.me/Deendayal_dhakad')
+                  InlineKeyboardButton('⚜️ Donate me ⚜️', url='https://graph.org/file/2a78470d2e9439d52fe15.jpg')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
