5d6ba59fc9ecf3911340d8eefedbd10fed4f2ddc
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2023-12-11 14:53:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/deendayal422/AUTO-FILTER-BOT.git /AUTO-FILTER-BOT 
+  git clone https://github.com/Deendayal423/Advance-Auto-filter-Bot.git /Advance-Auto-filter-Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AUTO-FILTER-BOT
+  git clone $UPSTREAM_REPO /Advance-Auto-filter-Bot
 fi
-cd /AUTO-FILTER-BOT
+cd /Advance-Auto-filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
