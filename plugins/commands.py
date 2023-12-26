3afc1ce9fded622edae88b4934f02fa7210e23c0
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2023-12-26 08:01:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -251,7 +251,7 @@ async def start(client, message):
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
             await message.reply_text(
-                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>",
+                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !.</b>",
                 protect_content=True
             )
             await verify_user(client, userid, token)
