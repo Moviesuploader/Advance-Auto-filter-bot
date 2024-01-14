4672ad94f38fb437b32f32bfdadc9731ccd660f3
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-01-14 14:30:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -323,7 +323,7 @@ async def start(client, message):
                 btn = [[
                     InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
-                    InlineKeyboardButton('How to verify', url='https://youtu.be/LvE02ODPKMc?si=3_7ZB71pZ__LUyUb')
+                    InlineKeyboardButton('How to verify', url='https://t.me/Deendayal_Dhakad1')
                 ]]
                 await message.reply_text(
                     text="<b>You are not verified !\nKindly verify to continue !\n\nइस Bot से मूवी प्राप्त करने के लिए आपको Verify करना होगा नहीं तो आप मूवी प्राप्त नहीं कर सकते।</b>",
@@ -387,7 +387,7 @@ async def start(client, message):
                 btn = [[
                     InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
-                    InlineKeyboardButton('How to verify', url='https://youtu.be/LvE02ODPKMc?si=3_7ZB71pZ__LUyUb')
+                    InlineKeyboardButton('How to verify', url='https://t.me/Deendayal_Dhakad1')
                 ]]
                 await message.reply_text(
                     text="<b>You are not verified !\nKindly verify to continue !\n\nइस Bot से मूवी प्राप्त करने के लिए आपको Verify करना होगा नहीं तो आप मूवी प्राप्त नहीं कर सकते।</b>",
@@ -447,7 +447,7 @@ async def start(client, message):
         btn = [[
                     InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
-                    InlineKeyboardButton('How to verify', url='https://youtu.be/LvE02ODPKMc?si=3_7ZB71pZ__LUyUb')
+                    InlineKeyboardButton('How to verify', url='https://t.me/Deendayal_Dhakad1')
                 ]]
         await message.reply_text(
             text="<b>You are not verified !\nKindly verify to continue !\n\nइस Bot से मूवी प्राप्त करने के लिए आपको Verify करना होगा नहीं तो आप मूवी प्राप्त नहीं कर सकते।</b>",
