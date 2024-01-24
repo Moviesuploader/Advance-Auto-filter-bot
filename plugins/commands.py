cf2baf4117210e92e2ab2cdc9da8df73fba28811
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-01-24 21:28:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def start(client, message):
                   InlineKeyboardButton('⚜️ Join Updates Channel ⚜️', url='https://t.me/Deendayal_dhakad')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAJ6wGWxMGFoRtt73gJsMEy7IXEEN0MhAAIGBwACc7LBVBHH8bMK6dZANAQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
