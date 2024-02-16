5f683cfc3364bbe0b6673ff5001ac679c09f617e
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-02-16 13:30:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,9 +97,9 @@ async def start(client, message):
         await client.send_photo(
             chat_id=message.from_user.id,
             photo="https://graph.org/file/7478ff3eac37f4329c3d8.jpg",
-            caption="**Please Join My Updates Channel to use this Bot!**\n\n"
+            caption="**Please Join My Updates Channel to use this Bot!**\n"
                  "**Due to Overload, Only Channel Subscribers can use this Bot!**\n\n"
-            "आपने हमारे Update Channel को join नहीं कर रखा है।"
+            "**आपने हमारे Update Channel को join नहीं कर रखा है।**\n"
             "**Join Our Channel वाले बटन पर Click करें। और हमारे अपडेट चैनल को ज्वाइन करें। इसके बाद आप फिर से Try करें।.....**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
